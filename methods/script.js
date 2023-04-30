49480a03d49365f75ffbc89de8894323edfe002b
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-04-30 22:21:11 +0200

    js code is pushed

---
+++ methods/script.js
@@ -0,0 +1,31 @@
+// forEach VS for loop
+// break ni faqat for da ishlata olamiz
+// const letters = ["a","b","c"]
+// letters.forEach(function(element, index){
+//     console.log('index: ',index)
+//     console.log('element: ', element)
+// })
+
+//map()
+// const numbers = [1,2,3,4]
+// const squared = numbers.map((value)=>value*value)
+
+//filter()
+// const numbers = [9,1,8,2,5]
+// const result = numbers.filter((value)=> {return value>5 })
+
+// find find an element findIndex() find the index of element
+//const result = users.find(user=> user.name === "James")
+
+// every dan farqli tarzda some un orasida 1 ta ture or false bolsa yetadi
+// const hasFive = numbers.some(value=> value.numbers === 5)
+
+// every method hamma case true bo'lishi kk
+// const numbers = [5,1,3]
+// const allPositive = numbers.every((number)=>{
+//     return number >= 0
+// })
+// this equal to 
+// const allPositive = numbers.every(number => number >= 0)
+// console.log(allPositive)
+
---
+++ methods/string.js
@@ -0,0 +1,46 @@
+// creating strings
+//const str = 'string ex code bla bla'
+//const str1 = new String('str2')
+// str.length shows the length of strng
+// string character acces , characters are 0 index based, first character is in index 0
+// str.charAt(4) shu indexdagi characterni chiqaradi
+// indexOf() and lastIndexOf()
+//str.indexOf('code') bu holatda indexOf -> bu taragfga qarab qidiradi
+//str.lastIndexOf('code) bu holatda <- bu tarafga qarab qidiradi
+// str.indexOf('code', 4) 4 chi indexdan boshlab -> tarafga
+// str.lastIndexOf('code', 10) 10 chi dan boshlab <- bu tarafga
+// str.indexOf('code', -5) 0 elementdan boshlab
+// str.lastindexOf('code', -5) -1 qaytaradi javob chiqarmidi
+
+//str.slice(4)  4-indexdagidan -> qarab kesibberadi
+//str.substring(4) 4-indexdan boshlidi -> oxirigaca kesib beradi
+//str.split('a')  a ni olib tashlab qolgan hamma elementni bitta bitta bolib element qilib qoyadi
+// ['string', 'ex', 'code' , 'bl', 'bl'] javob ciqadi
+// str.includes('ex') true qaytaradi sabab ex bor
+// str.includes('ex', 9) false chiqaradi
+// str.startsWith('ex') false qaytaradi
+//str.stratsWith('ex',8) true beradi
+// endsWith()
+// concat()
+const str = ' hello '
+// const str2 = 'world'
+// const newEl = str1.concat(str2)
+// const newEls = str1.concat(' '. str2)
+// repeat('el') nechta shunaqa element borligi
+// str.trim() white space remove qilinadi
+// str.trimStart() -> da whitespace qoshadi va str.trimEnd() <- bu yo'nalishda qo'shadi bor
+//str.padStart and str.padEnd()  bular white space qo'shadi
+//str1.localCompare('str2) 
+// str.search(regexp)
+//str.replace()
+//str.replaceAll()
+//str.charCodeAt()
+//str.codePointAt()
+ //str.toLocaleUpperCase()
+// str.toString() somehow = str.valueOf()
+// String.raw(strings, ...substitutions) 
+
+
+
+
+
