c5bbe553ca03b47504da75fb87e89dd0e4d66ba4
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-05-03 13:14:33 +0200

    lection notes from Frontend Masters

--- methods/test.js
+++ methods/test.js
@@ -1,1 +1,6 @@
-console.log("test")
+//types
+// primative, abstract operations, coercion, equality, typescript
+// undefined vs undeclared vs uninitialized
+//NaN 
+// use new : object(),array(),function(),date(),regexp(),error()
+// don't use new string(), number(), boolean()
