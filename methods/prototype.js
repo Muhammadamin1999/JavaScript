9af5abb848b3afcd88bf1353935c88395e4950cd
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-05-09 13:03:37 +0200

    MDN docs notes

---
+++ methods/prototype.js
@@ -0,0 +1,28 @@
+// const page = document.getElementById("prototype")
+// const arr = ["a","b","c"]
+
+// for(letter of arr){
+//     const li = document.createElement("li")
+//     li.textContent = letter
+//     page.appendChild(li)
+// }
+// const fruits = ["apple", "banana", "grapes", "mango", "orange"];
+// function filterItems(arr,query){
+//     return arr.filter((el)=>el.toLowerCase().includes(query.toLowerCase()))
+// }
+
+// console.log(filterItems(fruits, "ap")); // ['apple', 'grapes']
+// console.log(filterItems(fruits, "an")); // ['banana', 'mango', 'orange']
+// const arr = [1,2,2,4,5,5,4,3,7]
+// const set = new Set(arr.toString())
+// const result= Array.from(arr)
+// console.log(result)
+// const myArray = ["a", "b", "a", "b", "c", "e", "e", "c", "d", "d", "d", "d"];
+// const result = myArray.reduce((accumlator, currentValue)=>{
+//     if(!accumlator.includes(currentValue) ){
+//         return [...accumlator,currentValue]
+//     }
+//     return accumlator
+// },[])
+
+// console.log(result);
