6b8d3bc8d82d0ddb055f2bf5451cd28b3984d4cc
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-05-12 14:49:34 +0200

    drawings using JS

--- methods/drawings.js
+++ methods/drawings.js
@@ -138,16 +138,103 @@
 //     }
 //     console.log(arr)
 // }
-let n = 5
-let start = 1
-for(let i=0; i<n; i++){
-    var arr = []
-   if(i%2 === 0) start = 1
-   else start = 0
-   for(let j=0; j<=i; j++)
-   {
-      arr += start
-      start = 1-start
-   }
-   console.log(arr)
-}
+// let n = 5
+// let start = 1
+// for(let i=0; i<n; i++){
+//     var arr = []
+//    if(i%2 === 0) start = 1
+//    else start = 0
+//    for(let j=0; j<=i; j++)
+//    {
+//       arr += start
+//       start = 1-start
+//    }
+//    console.log(arr)
+// }
+// let n = 4
+// let space = 2*(n-1)
+
+// for(let i=1; i <= n; i++){
+//     var arr = []
+// for(let j=1; j<=i; j++)
+// {
+//     arr +=j
+// }
+// for( let k =1; k<=space; k++){
+//     arr+=' '
+// }
+
+// for(let j=i; j>0; j--)
+// {
+//     arr +=j
+// }
+
+// console.log(arr)
+// space -=2
+// }
+// let n=5
+// let num =1
+// for(let i=1; i<=n ; i++){
+//     var arr = []
+//     for(let j=1; j<=i; j++){
+//         arr += num
+//         num++
+//     }
+//     console.log(arr)
+// }
+// let n = 5
+
+// for(let i =1; i<=n; i++){
+//     var arr = []
+//     for(let j= 1; j<= i; j++){
+//         let res = `0x00${40 + j}`
+//         arr += String.fromCodePoint(res)
+//     }
+//     console.log(arr)
+// }
+// let n=5
+
+// for(let i =n; i<= n; i--){
+//     var arr= []
+//     for(let j=i; j>0; j--){
+//         let res = `0x00${46 - j}`
+//         arr += String.fromCodePoint(res)
+        
+//     }
+//     console.log(arr)
+// }
+// let n=5
+
+// for(let i =1; i<= n; i++){
+//     var arr= []
+//     for(let j=i; j>=0; j--){
+//         let res = `0x00${40 + i}`
+//         arr += String.fromCodePoint(res)
+//     }
+//     console.log(arr)
+// }
+// let n=5
+// for(let i=0; i<n; i++){
+//     var arr=[]
+//     var change = 0
+
+//     let breakponint = Math.ceil((2*i+1)/2)
+//     for(let j=0; j<n-i-1; j++){
+//         arr += ' '
+        
+//     }
+//     for(let k=1; k<= 2*i +1; k++){
+//         if( k<= breakponint) change++
+//         else change --
+//         let res = '0x00' + (40 + change)
+        
+//         arr+= String.fromCodePoint(res)
+        
+//     }
+
+//     for(let j=0; j<n-i-1; j++){
+//         arr += ' '
+        
+//     }
+//     console.log(arr)
+// }
