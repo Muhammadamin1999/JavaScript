dca988e1dab111d93fddbaecc21b67e22d1f19c1
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-06-05 23:35:42 +0200

    deep js

--- methods/deep.js
+++ methods/deep.js
@@ -73,3 +73,10 @@ class anotherWorkshop extends workshop{
 }
 var JSrecentParts = new anotherWorkshop("Kyle")
 JSrecentParts.speakUp("checking....")
+
+let promise = new Promise(function(resolve, reject) {
+    // the function is executed automatically when the promise is constructed
+  
+    // after 1 second signal that the job is done with the result "done"
+    setTimeout(() => resolve("done"), 1000);
+  });
