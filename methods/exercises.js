d507fc092e0fd1fa3f010d0cd8385253094e1a9a
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-06-24 11:17:23 +0200

    preparation for lecture exam algorithms

---
+++ methods/exercises.js
@@ -0,0 +1,84 @@
+// function reversestring(str){
+//      return Array.from(str).reverse().join("")
+// }
+// const ans = reversestring("Let\'s start")
+// console.log(ans)
+// let str = 'Hello'
+// let score = ''
+// //return str.split('').reverse().join()
+// for(let i = Array.from(str).length - 1; i >= 0; i--){
+//     score = score + Array.from(str)[i]  
+// }
+// console.log(score)
+// let str = "Hello"
+// let reversed = ''
+// for(let char of str){
+//     reversed = char + reversed
+// }
+// let str = 'Hello'
+// let score = ''
+// str.split('').forEach(char=>{score=char+score})
+// let str = 'Hello'
+// return str.split("").reduce((revString,char)=>char + revString,"")
+// function ispalindrome(str){
+//     let arr1= Array.from(str).reverse()
+//     let arr2= Array.from(str)
+//     return arr1.every(item=>(arr2.includes(item))) && arr2.every(item=>(arr1.includes(item)))
+// }
+//  const ans = ispalindrome("Arra")
+//  console.log(ans)
+
+// function reverseInteger(num){
+// return  Array.from(num.toString()).reverse().join("")
+// }
+// const ans = reverseInteger(123)
+// console.log(ans)
+
+// let str = "Hello World"
+// console.log(str.toUpperCase())
+// for(let i = 0; i <= 6; i++){
+//     for(let j = 0; j <= i; j++){
+//       console.log('#');
+//     };
+   
+//  };
+
+// drawing triangle
+// for(let a = "#"; a.length<8; a+="#")
+// {
+//     console.log(a)
+// }
+
+// fizz buzz
+// for(let i = 1; i<=100; i++){
+//     if(i%15 === 0){
+//      console.log('FizzBuzz');
+//     }else if(i%3 === 0){
+//     console.log('Fizz');
+//     }else if(i%5 === 0){
+//       console.log('Buzz');
+//     }else{
+//     console.log(i)
+//     };
+//   };
+
+// let arr = []
+// for(let i = 0; i<8; i++){
+//     for(let j = 0; j<8; j++){
+//         if(i%2 === 0){
+//             if(j%2 === 0){
+//                 arr[[i][j]] = 1
+//             }else{
+//                 arr[[i][j]] = 2
+//             }
+//         }else{
+//             if(j%2 === 1){
+//                 arr[[i][j]] = 2
+//             }else{
+//                 arr[[i][j]] = 1
+//             } 
+//         }
+//     }
+// }
+// console.log(arr)
+
--- methods/function.js
+++ methods/function.js
@@ -1,4 +1,93 @@
 //IIFE
-(function(){
-    return x*x
-})
+// (function(){
+//     return x*x
+// })
+
+// const todos =[
+//     {
+//         id:1,
+//         name: 'Frank',
+//         age:23,
+//         isCompleted: true
+//     },
+//     {
+//         id:2,
+//         name: "Doll",
+//         age:25,
+//         isCompleted: false
+//     }
+// ]
+// for(let i=0; i<todos.length; i++){
+//     console.log(todos[i].id)
+// }
+
+// for(value of todos){
+//     console.log(value.id)
+// }
+
+// const todoCompeted = todos.filter((todo)=>{
+//     return todo.isCompleted === true
+// }).map((todo)=> {return todo.name})
+
+// console.log(todoCompeted)
+
+// const x = 9;
+
+// const color = x > 10 ?"red" : "blue";
+
+// switch(color){
+//     case("red"): 
+//     console.log(`color is ${color}`);
+//     break;
+//     case("blue"):
+//     console.log(`color is ${color}`);
+//     break;
+//     default:
+//         console(`there is no such ${color}`)
+
+// }
+
+// OOP
+// function Person(name, surname, dob){
+//     this.name = name,
+//     this.surname = surname,
+//     this.dob = new Date(dob)
+//     this.getBirthDate = function(){
+//         return this.dob.getFullYear()
+//     }
+//     this.fullName = function(){
+//         return `${this.name} ${this.surname}`
+//     }
+// }
+
+// prototype
+// function Person(name, surname, dob){
+//     this.name = name,
+//     this.surname = surname,
+//     this.dob = new Date(dob)
+
+// }
+// Person.prototype.getBirthDate = function(){
+//     return this.dob.getFullYear()
+// }
+// Person.prototype.fullName = function(){
+//     return `${this.name} ${this.surname}`
+// }
+
+// class
+class Person{
+   constructor(name, surname, dob){
+    this.name = name,
+    this.surname = surname,
+    this.dob = new Date(dob)
+   }
+    getBirthDate = function(){
+        return this.dob.getFullYear()
+    }
+    fullName = function(){
+        return `${this.name} ${this.surname}`
+}
+}
+const person1 = new Person("Jamil", "Akbar", "3-6-1998")
+// console.log(person1.dob.getDate())
+console.log(person1.fullName())
