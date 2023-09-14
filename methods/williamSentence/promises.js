a65120b4c3a09d03b3c7256d1d9be7ab9350a87f
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-09-14 23:38:56 +0200

    promises exercises

---
+++ methods/williamSentence/promises.js
@@ -0,0 +1,50 @@
+
+// challange 1
+
+// function sayHello(){
+//     setTimeout(()=>{
+//         console.log('hello')
+//     },1000)
+// }
+// sayHello();
+
+// challange 2
+
+// var promise = new Promise(function (resolve,reject){
+//     setTimeout(resolve,1000);
+// });
+
+// function displayResolve(){
+//     console.log('Challange 2', 'Resolved');
+// }
+
+// promise.then(displayResolve);
+
+// Challange 3
+
+// promise = new Promise(function(resolve,reject){
+
+//     reject();
+// });
+
+// function displayRejected(){
+//     console.log('Rejected');
+// }
+
+// promise.catch(displayRejected);
+
+// challange 4
+
+// promise = new Promise(function(resolve, reject){
+//     resolve();
+// });
+
+// promise.then(function(){
+//     console.log("promise has been resolved ")
+// })
+
+// console.log('I am not promise');
+
+// challange 5
+
+
