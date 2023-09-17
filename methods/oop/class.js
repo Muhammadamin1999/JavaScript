b60e6c6cb95b6b12f40ce445e47ca8835d8ef7db
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-09-17 23:45:06 +0200

    js oop

--- methods/oop/class.js
+++ methods/oop/class.js
@@ -4,16 +4,39 @@ class User{
     constructor(email, name){
         this.email = email;
         this.name = name;
+        this.score = 0; 
     }
     login(){
         console.log(this.email, "just logged in");
+        return this;
     }
     logout(){
         console.log(this.email, "just logged out");
+        return this;
     }
+    updateScore(){
+        this.score++;
+        console.log(this.email, "score is now", this.score);
+        return this;
+    }
+
 }
+
+class Admin extends User{
+       deleteUser(user){
+            users = users.filter(u => {
+                return u.email != user.email;
+            });
+       }
+}
+
 var userOne = new User('ryu@gmail.com',"ryu");
 var usertwo = new User('yoshi@gmail.com', 'Yoshi');
+var admin = new Admin("shaun@gamil.com", "shaun");
+
+var users = [userOne, usertwo];
+admin.deleteUser(usertwo);
+console.log(userOne)
 
 // userOne.login();
 // usertwo.logout();
@@ -26,6 +49,8 @@ var usertwo = new User('yoshi@gmail.com', 'Yoshi');
 // sets the value of this to be the new empty obj
 // calls the constructor method
 
-// method chaining
+//userOne.login().updateScore().updateScore().logout(); // method chaining
 
+
+// class inheritance
 
