27cc5146ed65e59bf1946ae6818a919a8fc78ec5
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-09-17 23:28:55 +0200

    js oop

--- methods/oop/class.js
+++ methods/oop/class.js
@@ -1,2 +1,31 @@
 // if we need to create one more than object
 
+class User{
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+    }
+    login(){
+        console.log(this.email, "just logged in");
+    }
+    logout(){
+        console.log(this.email, "just logged out");
+    }
+}
+var userOne = new User('ryu@gmail.com',"ryu");
+var usertwo = new User('yoshi@gmail.com', 'Yoshi');
+
+// userOne.login();
+// usertwo.logout();
+// console.log(userOne);
+// console.log(usertwo);
+
+
+// the new keyword
+// creates a new empty obj {}
+// sets the value of this to be the new empty obj
+// calls the constructor method
+
+// method chaining
+
+
