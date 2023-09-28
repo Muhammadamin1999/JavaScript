9c30069689219c46666fa0e8601dc192bff6cec4
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-09-28 23:21:33 +0200

    leetcode exercise

---
+++ methods/method/callbacks.js
@@ -0,0 +1,22 @@
+function fetchData() {
+    return new Promise((resolve, reject) => {
+      // Simulate an asynchronous operation (e.g., fetching data)
+      setTimeout(() => {
+        const data = { message: 'Data fetched successfully' };
+        resolve(data);
+      }, 1000);
+    });
+  }
+  
+  fetchData()
+    .then((data) => {
+      console.log(data.message);
+      return 'Additional data';
+    })
+    .then((additionalData) => {
+      console.log(additionalData);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
